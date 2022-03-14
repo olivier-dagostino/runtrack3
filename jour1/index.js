b7a3324a5e290d8f00f2e657737ac600c3c7df22
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-03-14 10:13:52 +0100

    test

---
+++ jour1/index.js
@@ -0,0 +1,37 @@
+'use strict'; // permer de renvoyer plus d'erreur détaillé 
+
+document.addEventListener('DOMContentLoaded', function loaded() {
+
+    //JS
+    //window.alert('');
+    //console.log('');
+    
+
+    // Déclarer un variable 
+
+        //SCOPE = A quelle endroit ma variable est accesible 
+
+        // var myVariable = 5 + 5 ;
+        // let mySecondVariable ;
+
+        // // ...
+
+        // myVariable = 'Hello JS';
+        // mySecondVariable = 5 + 5 ;
+
+        // var myVariable;
+
+        // for (let i = 0; i < 5; i++) {
+            
+        //     myVariable =+ 'ok';
+        // }
+
+        // console.log(myVariable);
+
+        // let myVariable ='';
+        // const myConst ='test';
+
+        
+    
+
+});
