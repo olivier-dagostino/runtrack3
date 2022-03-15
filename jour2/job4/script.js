d81a7e9ef08b9957393ba741f5547d73e7703c13
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-03-15 18:40:14 +0100

    job4

---
+++ jour2/job4/script.js
@@ -0,0 +1,8 @@
+var textarea = document.getElementById('keylogger');
+
+document.addEventListener('keypress',function() {
+    var ancienmot = textarea.value;
+    var lettre = event.key
+    textarea.value =  ancienmot + lettre ;
+});
+
