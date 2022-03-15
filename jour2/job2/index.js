986bc06743bf80802659ff15f67c2e8f15317ee9
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2022-03-15 14:43:25 +0100

    up

--- jour2/job2/index.js
+++ jour2/job2/index.js
@@ -5,16 +5,18 @@ dessus, un <article> contenant le texte suivant est ajouté au contenu de la
 page : “L'important n'est pas la chute, mais l'atterrissage.”. Si on clique à 
 nouveau sur ce bouton, l’article disparaît. L’apparition / Disparition doivent 
 être gérées dans une fonction nommée “showhide()”. */
+document.addEventListener('DOMContentLoaded', function loaded(){
+    
+    var button = document.querySelector('button'),
+        article = document.querySelector('article');
 
-function showhide() {
+    button.addEventListener('click', function showhide(){
 
-      document.querySelector("article").classList.toggle("hidden");
-  }
-  
-  var button = document.querySelector('#button');
-  
-  button.addEventListener('click', showhide); 
+        article.classList.toggle("article");
+
+    });
 
 
+})
 
 
